6115c18c8ecf6e7b8be04e13ae105f86f8ced762
Shortytheman <shor7ty@gmail.com>
2021-10-11 14:07:50 +0200

    første commit

---
+++ src/Main.java
@@ -0,0 +1,4 @@
+public class Main {
+
+  Pizza pizza1 = new Pizza("Skinke","ost",76);
+}
---
+++ src/Pizza.java
@@ -0,0 +1,47 @@
+public class Pizza {
+
+  int pris;
+  String ingrediens;
+  String ingrediens1;
+  String ingrediens2;
+  String ingrediens3;
+  String ingrediens4;
+  String ingrediens5;
+
+  Pizza(String ingrediens, String ingrediens1, int pris){
+    this.ingrediens = ingrediens;
+    this.ingrediens1 = ingrediens1;
+    this.pris = pris;
+  }
+  Pizza(String ingrediens, String ingrediens1, String ingredisens2, int pris){
+    this.ingrediens = ingrediens;
+    this.ingrediens1 = ingrediens1;
+    this.ingrediens2 = ingredisens2;
+    this.pris = pris;
+  }
+  Pizza(String ingrediens, String ingrediens1, String ingredisens2, String ingredisens3, int pris){
+    this.ingrediens = ingrediens;
+    this.ingrediens1 = ingrediens1;
+    this.ingrediens2 = ingredisens2;
+    this.ingrediens3 = ingredisens3;
+    this.pris = pris;
+  }
+  Pizza(String ingrediens, String ingrediens1, String ingredisens2, String ingredisens3, String ingredisens4, int pris){
+    this.ingrediens = ingrediens;
+    this.ingrediens1 = ingrediens1;
+    this.ingrediens2 = ingredisens2;
+    this.ingrediens3 = ingredisens3;
+    this.ingrediens4 = ingredisens4;
+    this.pris = pris;
+  }
+  Pizza(String ingrediens, String ingrediens1, String ingredisens2, String ingredisens3, String ingredisens4, String ingredisens5, int pris){
+    this.ingrediens = ingrediens;
+    this.ingrediens1 = ingrediens1;
+    this.ingrediens2 = ingredisens2;
+    this.ingrediens3 = ingredisens3;
+    this.ingrediens4 = ingredisens4;
+    this.ingrediens5 = ingredisens5;
+    this.pris = pris;
+  }
+
+}
